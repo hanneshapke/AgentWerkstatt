d702285fa799f05b5d95de26487b720ab4b0da4e
Hannes Hapke <hannes.hapke@gmail.com>
2025-07-27 17:05:39 -0700

    updated readme

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev29+gb78bbd2.d20250727'
-__version_tuple__ = version_tuple = (0, 1, 'dev29', 'gb78bbd2.d20250727')
+__version__ = version = '0.1.1.dev0+g80f4d1b.d20250728'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'g80f4d1b.d20250728')
